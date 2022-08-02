7bd6b693f30afdeff3f8a0b127a50cdf0759479f
holethanhbao030695 <holethanhbao030695@gmail.com>
2022-08-02 13:58:37 +0700

    add file

---
+++ main.js
@@ -0,0 +1,26 @@
+// const headerMenu = document.querySelector(".header");
+// const brandLogo = document.querySelector(".brand-logo");
+// const menuLink = document.querySelectorAll(".menu-link");
+
+
+// window.addEventListener("scroll", function () {
+//   if (
+//     document.body.scrollTop > 150 ||
+//     document.documentElement.scrollTop > 150
+//   ) {
+//     // headerMenu.classList.add("white-bg");
+//     brandLogo.classList.add("hiden");
+//   } else {
+//     // headerMenu.classList.remove("white-bg");
+//     brandLogo.classList.remove("hiden");
+//   }
+// });
+
+// menuLink.forEach(e => {
+//   e.addEventListener("click",()=> {
+//     for(let item of menuLink) {
+//       item.classList.remove("tick")
+//     }
+//     e.classList.add("tick")
+//   })
+// })
